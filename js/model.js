c0ca3982caaaa6e779bfd8501ba676c822708e6d
LeoPaez <lpaezbusiness@gmail.com>
2022-09-19 20:48:01 -0300

    Se suben cambios

--- js/model.js
+++ js/model.js
@@ -3,14 +3,14 @@ export default class Model {
         this.view = null;
         this.todos = JSON.parse(localStorage.getItem("todos"));
         if(!this.todos || this.todos.length < 1) {
-            // this.todos = [
-            //     {
-            //         id: 0,
-            //         title: "Learn JS",
-            //         description: "Watch JS Tutorials",
-            //         completed: false
-            //     }
-            // ]
+            this.todos = [
+                {
+                    id: 0,
+                    title: "Learn JS",
+                    description: "Watch JS Tutorials",
+                    completed: false
+                }
+            ]
             this.currentId = 1;
         } else {
             this.currentId = this.todos[this.todos.length - 1].id + 1
